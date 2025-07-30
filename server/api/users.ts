0a7522fc8578ee1d4ce60a04d2f595729e5277ab
AndreySamaev <andreysamaev@gmail.com>
2025-07-30 19:20:41 +0300

    fix data folder

--- server/api/users.ts
+++ server/api/users.ts
@@ -3,7 +3,7 @@ import { join } from 'path'
 import type { UserWithPassword, User } from '~/types/User'
 
 export default defineEventHandler(async () => {
-    const filePath = join(process.cwd(), 'public', 'users.json')
+    const filePath = join(process.cwd(), 'data', 'users.json')
     const json = await readFile(filePath, 'utf-8')
     const users: UserWithPassword[] = JSON.parse(json)
 
--- server/utils/getUsers.ts
+++ server/utils/getUsers.ts
@@ -3,7 +3,7 @@ import { join } from 'path'
 import type { UserWithPassword } from '~/types/User'
 
 export async function getUsers(): Promise<UserWithPassword[]> {
-    const filePath = join(process.cwd(), 'public', 'users.json')
+    const filePath = join(process.cwd(), 'data', 'users.json')
     const json = await readFile(filePath, 'utf-8')
     return JSON.parse(json)
 }
