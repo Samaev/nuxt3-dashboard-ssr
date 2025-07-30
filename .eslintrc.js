78c404a322bc47543198a2aca380f9db40dd2201
AndreySamaev <andreysamaev@gmail.com>
2025-07-30 19:51:11 +0300

    linter configurated

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: [
+        '@nuxt/eslint-config',
+    ],
+    rules: {
+        'vue/multi-word-component-names': 'off',
+    },
+}
